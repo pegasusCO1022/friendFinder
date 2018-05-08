3da8966badd2e7a457079d32d1091d4dddc2ffbf
Adam <spiderwebb2290@gmail.com>
2018-05-08 15:11:15 -0600

    having a hard time with the javascript logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,42 @@
 // Displays friends array list 
+var friendsArray = require("../data/friends.js")
+
+module.exports = function (app){
+
+
 app.get("/api/friends", function (req, res) {
     return res.json(friendsArray);
 });
+
 app.post("/api/friends", function (req, res) {
     
     let surveyResults = req.body;
 
-    console.log(surveyResults);
-
-    surveyResults.push(friendsArray);
+    friendsArray.push(surveyResults);
 
     res.json(friendsArray);
+
+    for(var i = 0; i < friendsArray.length; i++){
+        var userScores = friendsArray.scores[i];
+        for(var j = 0; j < userScores.length; j++){
+            parseInt(userScores);
+        }
+    }
+
+    console.log(userScores);
+
+    // for(var i = 0; i < friendsArray.length; i++){
+    //     console.log()
+    //     }
+
+    // }
+    // loop through friends array (nested array)
+    // go through each friend first array (nested array for scores)
+    // math.abs absolute difference
+    // new array for scores and compare scores
+    // res.json new friend object
+
+   
 });
+
+}
--- server.js
+++ server.js
@@ -2,27 +2,24 @@
 // =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
-var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3030;
+
+
+// var friendsArray = require("./app/data/friends.js")
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 // Import data files from seperate JS
 // var friendsArray = require('./friends.js');
 // Basic route that sends the user first to the AJAX Page
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "app/public/home.html"));
-});
-app.get("/survey", function (req, res) {
-    res.sendFile(path.join(__dirname, "app/public/survey.html"));
-});
 // Displays all survey results 
-app.get("/api/friends", function (req, res) {
-    return res.json(friendsArray);
-});
 
 // listening and letting us know the server is up and running 
 app.listen(PORT, function () {
